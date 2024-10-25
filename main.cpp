a83958a3c23a9f60ee42190e329a3fafd1b4c9ca
Jay <jay.jrjren@gmail.com>
2024-10-25 10:06:01 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
