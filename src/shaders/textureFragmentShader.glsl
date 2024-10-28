d1be0796604844bf8c0689f29a8dd8375e810b18
Jay <jay.jrjren@gmail.com>
2024-10-28 14:55:56 -0400

    Fix CMake, add default modulate color to texture frag shader

--- src/shaders/textureFragmentShader.glsl
+++ src/shaders/textureFragmentShader.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 in vec2 TexCoord;
 
-uniform vec4 modulateColor;
+uniform vec4 modulate = vec4(1.0, 1.0, 1.0, 1.0);
 uniform sampler2D texture1;
 
 void main()
 {
-    gl_FragColor  = texture(texture1, TexCoord) * modulateColor;
+    gl_FragColor = texture(texture1, TexCoord) * modulate;
 }
